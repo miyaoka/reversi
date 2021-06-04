1dfdc7acb4c92078c7b7fb8aff73fdbd1718f93b
Masaya Kazama <miyan@t-p.jp>
2021-06-04 13:09:46 +0900

    add github link

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,28 @@ import TheCanvas from "./components/TheCanvas.vue";
 </script>
 
 <template>
-  <TheCanvas />
+  <div>
+    <TheCanvas />
+    <div
+      class="
+        absolute
+        right-2
+        top-2
+        w-12
+        h-12
+        bg-white
+        rounded-full
+        p-2
+        hover:bg-yellow-400
+      "
+    >
+      <a
+        href="https://github.com/miyaoka/reversi"
+        target="_blank"
+        title="repository"
+      >
+        <img src="./assets/GitHub-Mark-64px.png" />
+      </a>
+    </div>
+  </div>
 </template>
